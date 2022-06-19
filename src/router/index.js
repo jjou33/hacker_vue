336a91d68011d5f3b6ad3f35709505d167780c5f
hyunwoo <zanda33@naver.com>
2022-06-19 22:22:11 +0900

    FEAT: 라우팅 설치 및 설정
    - Vue Router 설치 및 기본 ask, news, jobs 컴포넌트 라우팅 설정 완료

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import NewsView from "../views/NewsView.vue";
+import AskView from "../views/AskView.vue";
+import JobsView from "../views/JobsView.vue";
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+  routes: [
+    {
+      path: "/news", // url 주소
+      component: NewsView, // url 주소로 갔을 때 표시될 컴포넌트
+    },
+    {
+      path: "/ask",
+      component: AskView,
+    },
+    {
+      path: "/jobs",
+      component: JobsView,
+    },
+  ],
+});
