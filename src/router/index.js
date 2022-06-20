1ce2066133ee019d20d04f2f022b5b77ba916ab8
hyunwoo <zanda33@naver.com>
2022-06-20 23:11:53 +0900

    Feat: router link 설정
    - router link 설정 및 신규 컴포넌트 생성

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,17 @@ import VueRouter from "vue-router";
 import NewsView from "../views/NewsView.vue";
 import AskView from "../views/AskView.vue";
 import JobsView from "../views/JobsView.vue";
-
+import UserView from "../views/UserView.vue";
+import ItemView from "../views/ItemView.vue";
 Vue.use(VueRouter);
 
 export const router = new VueRouter({
+  mode: "history",
   routes: [
+    {
+      path: "/",
+      redirect: "/news",
+    },
     {
       path: "/news", // url 주소
       component: NewsView, // url 주소로 갔을 때 표시될 컴포넌트
@@ -19,6 +25,14 @@ export const router = new VueRouter({
     {
       path: "/jobs",
       component: JobsView,
+    },
+    {
+      path: "/user",
+      component: UserView,
+    },
+    {
+      path: "/item",
+      component: ItemView,
     },
   ],
 });
