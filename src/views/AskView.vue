6a235b11e8b5f4adb98a2a07c403a096228ef851
happycho <zanda33@naver.com>
2022-06-21 10:08:46 +0900

    Feat: axios 설치 및 적용
    - axios 패키지 설치 후 각 View 에 활용할 수 있도록 api 분리 및 view 에 적용 완료

--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -1,9 +1,23 @@
 <template>
-  <div>ask</div>
+  <div>
+    <div v-bind:key="item.id" v-for="item in asks">{{ item.title }}</div>
+  </div>
 </template>
 
 <script>
-export default {};
+import { fetchAskList } from "../api/index.js";
+export default {
+  data() {
+    return {
+      asks: [],
+    };
+  },
+  created() {
+    fetchAskList()
+      .then((res) => (this.asks = res.data))
+      .catch();
+  },
+};
 </script>
 
 <style></style>
--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,9 +1,25 @@
 <template>
-  <div>news</div>
+  <div>
+    <div v-bind:key="user.id" v-for="user in users">{{ user.title }}</div>
+  </div>
 </template>
 
 <script>
-export default {};
+import { fetchNewsList } from "../api/index.js";
+
+export default {
+  data() {
+    return {
+      users: [],
+    };
+  },
+  created() {
+    fetchNewsList()
+      .then((res) => (this.users = res.data))
+      .catch();
+  },
+  methods: {},
+};
 </script>
 
 <style></style>
