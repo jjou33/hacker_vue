015dd502a91ffdca762cc5a19a4bb47d1bc44b23
happycho <zanda33@naver.com>
2022-06-24 15:08:16 +0900

    Refactor: navigation guard 적용

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
     <tool-bar></tool-bar>
-    <transition name="page">
-      <router-view></router-view>
-    </transition>
+    <!-- <transition name="page"> -->
+    <router-view></router-view>
+    <!-- </transition> -->
     <spinner :loading="loadingStatus"></spinner>
   </div>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,12 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import UserView from "../views/UserView.vue";
 import ItemView from "../views/ItemView.vue";
-import createListView from "../views/CreateListView.js";
+import NewsView from "../views/NewsView.vue";
+import AskView from "../views/AskView.vue";
+import JobsView from "../views/JobsView.vue";
+// import createListView from "../views/CreateListView.js";
+import { emitter } from "../utils/mitt.js";
+import { store } from "../store/store.js";
 Vue.use(VueRouter);
 
 export const router = new VueRouter({
@@ -15,17 +20,56 @@ export const router = new VueRouter({
     {
       path: "/news", // url 주소
       name: "news",
-      component: createListView("NewsView"), // url 주소로 갔을 때 표시될 컴포넌트
+      // component: createListView("NewsView"), // url 주소로 갔을 때 표시될 컴포넌트
+      component: NewsView,
+      beforeEnter: (to, from, next) => {
+        emitter.emit("start:spinner");
+        store
+          .dispatch("FETCH_LIST", to.name)
+          .then(() => {
+            emitter.emit("end:spinner");
+            next();
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      },
     },
     {
       path: "/ask",
       name: "ask",
-      component: createListView("AskView"),
+      // component: createListView("AskView"),
+      component: AskView,
+      beforeEnter: (to, from, next) => {
+        emitter.emit("start:spinner");
+        store
+          .dispatch("FETCH_LIST", to.name)
+          .then(() => {
+            emitter.emit("end:spinner");
+            next();
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      },
     },
     {
       path: "/jobs",
       name: "jobs",
-      component: createListView("JobsView"),
+      // component: createListView("JobsView"),
+      component: JobsView,
+      beforeEnter: (to, from, next) => {
+        emitter.emit("start:spinner");
+        store
+          .dispatch("FETCH_LIST", to.name)
+          .then(() => {
+            emitter.emit("end:spinner");
+            next();
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      },
     },
     {
       path: "/user/:id",
